6236b90887dd819e0e941496e5ac96a332cdc7ae
Joseph Haase <joseph.haase.dev@gmail.com>
2023-09-23 17:37:48 -0300

    Adds the use of the register store and steps view

--- src/views/register/RegisterView.vue
+++ src/views/register/RegisterView.vue
@@ -1,24 +1,39 @@
 <template>
   <div id="register-view__container">
+    <RegisterSteps />
     <FormBox>
-      <RegisterStepOne v-if="true" />
-      <RegisterStepTwo v-else />
+      <RegisterStepOne v-if="isStepOne" />
+      <RegisterStepTwo v-if="isStepTwo" />
     </FormBox>
   </div>
 </template>
 
 <script lang="ts" setup>
+import { onUnmounted } from "vue";
+import { storeToRefs } from "pinia";
+import { useRegisterStore } from "@/stores/RegisterStore";
+
 import FormBox from "@/components/form-box/FormBox.vue";
 import RegisterStepOne from "./components/RegisterStepOne.vue";
 import RegisterStepTwo from "./components/RegisterStepTwo.vue";
+import RegisterSteps from "./components/RegisterSteps.vue";
+
+const registerStore = useRegisterStore();
+const { isStepOne, isStepTwo } = storeToRefs(registerStore);
+const { $reset } = registerStore;
+
+onUnmounted(() => {
+  $reset();
+});
 </script>
 
 <style lang="scss" scoped>
 #register-view__container {
   min-height: 100dvh;
-  padding: 24px;
+  padding: 32px 24px;
   display: flex;
   align-items: center;
   justify-content: center;
+  position: relative;
 }
 </style>

fbed8f918d089c1d9e9391ea4ed9a909ca341aa9
Joseph Haase <joseph.haase.dev@gmail.com>
2023-09-25 16:45:14 -0300

    Creates the step two

--- src/views/register/components/RegisterStepTwo.vue
+++ src/views/register/components/RegisterStepTwo.vue
@@ -1,3 +1,77 @@
-<template><div></div></template>
-<script lang="ts" setup></script>
-<style lang="scss" scoped></style>
+<template>
+  <div class="register-step-two__container">
+    <BackButton
+      class="register-step-two__back-button"
+      @click="backToStepOne"
+    />
+    <h1 class="register-step-two__title">Escolha seu avatar</h1>
+
+    <div class="register-step-two__buttons-container">
+      <div class="register-step-two__photo-button">
+        <img
+          src="/src/assets/images/poro.png"
+          alt="poro"
+        />
+      </div>
+
+      <AppButton is-full>Cadastrar</AppButton>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { useRegisterStore } from "@/stores/RegisterStore";
+
+import AppButton from "@/components/app-button/AppButton.vue";
+import BackButton from "@/components/back-button/BackButton.vue";
+
+const registerStore = useRegisterStore();
+const { changeStep } = registerStore;
+
+const backToStepOne = () => changeStep(1);
+</script>
+
+<style lang="scss">
+.register-step-two__container {
+  .register-step-two__back-button {
+    align-self: flex-start;
+  }
+}
+</style>
+
+<style lang="scss" scoped>
+.register-step-two__container {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  .register-step-two__title {
+    color: #fff;
+    font-size: 24px;
+    font-weight: 600;
+    line-height: normal;
+    margin: 36px 0;
+    text-align: center;
+  }
+
+  .register-step-two__buttons-container {
+    margin-bottom: 36px;
+    display: flex;
+    flex-direction: column;
+    gap: 36px;
+
+    .register-step-two__photo-button {
+      display: flex;
+      border: 3px solid #ffe500;
+      height: 128px;
+      width: 128px;
+      cursor: pointer;
+
+      img {
+        max-width: 100%;
+      }
+    }
+  }
+}
+</style>

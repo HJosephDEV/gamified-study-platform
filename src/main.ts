4ca4e8daf40aae23e032fba29b61419aa9f0f149
Joseph Haase <joseph.haase.dev@gmail.com>
2023-10-22 19:31:40 -0300

    Adds a middleware of authenticated route

--- src/main.ts
+++ src/main.ts
@@ -8,13 +8,12 @@ import { createPinia } from "pinia";
 
 import App from "./App.vue";
 import router from "./router";
-// import axios from "./plugins/axios";
+import "./router/middleware.ts";
 
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
-// app.use(axios);
 
 app.mount("#app");
 
